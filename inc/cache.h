be603370665e4148763c18a957d5cd88978d18cd
Giovanni Brignone <brigio345@gmail.com>
2021-05-04 08:13:52 +0200

    fix(cache): set correct variable name in pragma

--- inc/cache.h
+++ inc/cache.h
@@ -259,7 +259,7 @@ FILL_LOOP:		for (int off = 0; off < N_ENTRIES_PER_LINE; off++) {
 
 SPILL_LOOP:		for (int off = 0; off < N_ENTRIES_PER_LINE; off++) {
 #pragma HLS dependence inter variable=main_mem false
-#pragma HLS dependence inter variable=cache_mem false
+#pragma HLS dependence inter variable=_cache_mem false
 				main_line[off] = cache_line[off];
 			}
 
