c83c82973ee3503ca1f0653af0f26313fd06e9f9
Giovanni Brignone <brigio345@gmail.com>
2021-06-18 11:58:21 +0200

    refactor(address): simplify logic using bitwise operations

--- inc/address.h
+++ inc/address.h
@@ -1,70 +1,51 @@
 #ifndef ADDRESS_H
 #define ADDRESS_H
 
-#include "ap_int.h"
-
 template <size_t ADDR_SIZE, size_t TAG_SIZE, size_t SET_SIZE, size_t WAY_SIZE>
 class address {
 	private:
 		static const size_t OFF_SIZE = (ADDR_SIZE - (TAG_SIZE + SET_SIZE));
-		static const size_t LINE_ADDR_SIZE = (SET_SIZE + WAY_SIZE);
-		static const size_t CACHE_ADDR_SIZE = (LINE_ADDR_SIZE + OFF_SIZE);
+		static const unsigned int OFF_MASK = (~(-1U << OFF_SIZE));
+		static const unsigned int SET_MASK = (~(-1U << SET_SIZE));
+		static const unsigned int TAG_MASK = (~(-1U << TAG_SIZE));
 
 	public:
-		ap_uint<ADDR_SIZE> _addr_main;
-		ap_uint<(CACHE_ADDR_SIZE > 0) ? CACHE_ADDR_SIZE : 1> _addr_cache;
-		ap_uint<(LINE_ADDR_SIZE > 0) ? LINE_ADDR_SIZE : 1> _addr_line;
-		ap_uint<(TAG_SIZE > 0) ? TAG_SIZE : 1> _tag;
-		ap_uint<(SET_SIZE > 0) ? SET_SIZE : 1> _set;
-		ap_uint<(OFF_SIZE > 0) ? OFF_SIZE : 1> _off;
-		ap_uint<(WAY_SIZE > 0) ? WAY_SIZE : 1> _way;
+		unsigned int _addr_main;
+		unsigned int _addr_cache;
+		unsigned int _addr_line;
+		unsigned int _tag;
+		unsigned int _set;
+		unsigned int _off;
+		unsigned int _way;
 
-		address(ap_uint<ADDR_SIZE> addr_main): _addr_main(addr_main) {
-			_off = 0;
-			for (auto i = 0; i < OFF_SIZE; i++)
-				_off[i] = addr_main[i];
-			_set = 0;
-			for (auto i = 0; i < SET_SIZE; i++)
-				_set[i] = addr_main[i + OFF_SIZE];
-			_tag = 0;
-			for (auto i = 0; i < TAG_SIZE; i++)
-				_tag[i] = addr_main[i + OFF_SIZE + SET_SIZE];
-
-			_addr_cache = -1;
-			_addr_line = -1;
+		address(unsigned int addr_main): _addr_main(addr_main) {
+			_off = (addr_main & OFF_MASK);
+			_set = ((addr_main >> OFF_SIZE) & SET_MASK);
+			_tag = ((addr_main >> (OFF_SIZE + SET_SIZE)) & TAG_MASK);
 		}
 
-		address(ap_uint<(TAG_SIZE > 0) ? TAG_SIZE : 1> tag,
-				ap_uint<(SET_SIZE > 0) ? SET_SIZE : 1> set,
-				ap_uint<(OFF_SIZE > 0) ? OFF_SIZE : 1> off,
-				ap_uint<(WAY_SIZE > 0) ? WAY_SIZE : 1> way):
+		address(unsigned int tag, unsigned int set,
+				unsigned int off, unsigned int way):
 				_tag(tag), _set(set), _off(off) {
-			_addr_main = 0;
+			tag = (_tag << (SET_SIZE + OFF_SIZE));
+			set = (_set << OFF_SIZE);
 
-			for (auto i = 0; i < OFF_SIZE; i++)
-				_addr_main[i] = _off[i];
-			for (auto i = 0; i < SET_SIZE; i++)
-				_addr_main[i + OFF_SIZE] = set[i];
-			for (auto i = 0; i < TAG_SIZE; i++)
-				_addr_main[i + OFF_SIZE + SET_SIZE] = _tag[i];
+			_addr_main = (tag | set | _off);
 
 			set_way(way);
 		}
 
-		void set_way(ap_uint<(WAY_SIZE > 0) ? WAY_SIZE : 1> way) {
+		void set_way(unsigned int way) {
 			_way = way;
 
-			_addr_line = 0;
-			for (auto i = 0; i < WAY_SIZE; i++)
-				_addr_line[i] = _way[i];
-			for (auto i = 0; i < SET_SIZE; i++)
-				_addr_line[i + WAY_SIZE] = _set[i];
+			auto set = (_set << WAY_SIZE);
 
-			_addr_cache = 0;
-			for (auto i = 0; i < OFF_SIZE; i++)
-				_addr_cache[i] = _off[i];
-			for (auto i = 0; i < LINE_ADDR_SIZE; i++)
-				_addr_cache[i + OFF_SIZE] = _addr_line[i];
+			_addr_line = (set | _way);
+
+			set = (_set << (WAY_SIZE + OFF_SIZE));
+			way = (_way << OFF_SIZE);
+
+			_addr_cache = (set | way | _off);
 		}
 };
 
