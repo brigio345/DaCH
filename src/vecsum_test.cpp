4f8e6f05832b073d7a5dd3dd02c08adcf193c7ed
Giovanni Brignone <brigio345@gmail.com>
2021-07-04 14:38:21 +0200

    chore(test): remove calls to init

--- src/vecsum_test.cpp
+++ src/vecsum_test.cpp
@@ -26,8 +26,6 @@ void vecsum_cache(cache_a &a, int &sum) {
 	int tmp = 0;
 	int data;
 
-	a.init();
-
 VECSUM_LOOP:	for (int i = 0; i < N; i++) {
 #pragma HLS pipeline
 #pragma HLS unroll factor=RD_PORTS
@@ -58,8 +56,6 @@ extern "C" void vecsum_top(int a[N], int &sum) {
 	a_cache.run(a);
 	vecsum_syn(a_cache, sum);
 #else
-	a_cache.init();
-
 	std::thread cache_thread([&]{a_cache.run(a);});
 	std::thread vecsum_thread([&]{vecsum_cache(a_cache, sum);});
 
