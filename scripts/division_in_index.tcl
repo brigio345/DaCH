f4d8d321824b55c9c29ebfe922022d1daa0519f5
Giovanni Brignone <brigio345@gmail.com>
2022-04-06 10:40:50 +0000

    test: remove unneeded from anonymous branch

--- scripts/division_in_index.tcl
+++
@@ -1,8 +0,0 @@
-source scripts/run_hls.tcl
-
-set top "division_in_index"
-set design_files {"test/division_in_index_test.cpp"}
-set tb_files {"test/division_in_index_test.cpp"}
-
-run_hls $top $design_files $tb_files xcvu9p-flga2104-2-i
-
--- scripts/unaligned_start_index_1.tcl
+++
@@ -1,8 +0,0 @@
-source scripts/run_hls.tcl
-
-set top "unaligned_start_index_1"
-set design_files {"test/unaligned_start_index_1_test.cpp"}
-set tb_files {"test/unaligned_start_index_1_test.cpp"}
-
-run_hls $top $design_files $tb_files xcvu9p-flga2104-2-i
-
