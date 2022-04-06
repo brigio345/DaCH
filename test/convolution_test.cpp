6708c3bb6deb0ba20bdf8e7fc581c2a86c4268ce
Giovanni Brignone <brigio345@gmail.com>
2022-04-06 10:35:06 +0000

    test: update MatMult and Conv2D

--- test/convolution_test.cpp
+++ test/convolution_test.cpp
@@ -10,18 +10,18 @@
 //#define GLOBAL
 //#define LOCAL
 
-#define N_ROWS 32
-#define N_COLS 32
-#define N_KER_ROWS 3
-#define N_KER_COLS 3
-#define N_KER_ROWS_ROUND 4
-#define N_KER_COLS_ROUND 4
+#define N_ROWS 16
+#define N_COLS 16
+#define N_KER_ROWS 2
+#define N_KER_COLS 2
+#define N_KER_ROWS_ROUND 2
+#define N_KER_COLS_ROUND 2
 
 static const size_t RD_PORTS = 1;
 
-typedef cache<int, true, false, RD_PORTS, (N_ROWS * N_COLS), 1, 4, 32, false, 0, 9> cache_in;
-typedef cache<int, true, false, RD_PORTS, (N_KER_ROWS_ROUND * N_KER_COLS_ROUND), 1, 1, 16, false, 1, 9> cache_ker;
-typedef cache<int, false, true, 1, (N_ROWS * N_COLS), 1, 1, 32, false, 0, 1> cache_out;
+typedef cache<int, true, false, RD_PORTS, (N_ROWS * N_COLS), 1, 1, 16, false, 1, 16 / RD_PORTS, false, 6> cache_in;
+typedef cache<int, true, false, RD_PORTS, (N_KER_ROWS_ROUND * N_KER_COLS_ROUND), 1, 1, N_KER_COLS, false, N_KER_ROWS, 1, false, 1> cache_ker;
+typedef cache<int, false, true, 1, (N_ROWS * N_COLS), 1, 1, 32, false, 0, 0, false, 4> cache_out;
 
 template <typename T, typename U, typename V>
 	void convolution(T in, U kernel, V out) {
@@ -36,6 +36,7 @@ J_LOOP:			for (auto j = 0; j < N_COLS; ++j) {
 M_LOOP:				for (auto m = 0; m < N_KER_ROWS; ++m) {
 N_LOOP:					for (auto n = 0; n < N_KER_COLS; ++n) {
 #pragma HLS pipeline II=1
+#pragma HLS unroll factor=RD_PORTS
 						// index of input signal, used for checking boundary
 						const auto ii = i + (kCenterY - m);
 						const auto jj = j + (kCenterX - n);
@@ -43,14 +44,80 @@ N_LOOP:					for (auto n = 0; n < N_KER_COLS; ++n) {
 						// ignore input samples which are out of bound
 						if ((ii >= 0) && (ii < N_ROWS) && (jj >= 0) && (jj < N_COLS))
 							tmp += (in[ii * N_COLS + jj] * kernel[m * N_KER_COLS + n]);
+					}
+				}
+				out[i * N_COLS + j] = tmp;
+			}
+		}
+	}
 
-						if (m == (N_KER_ROWS - 1) && (n == (N_KER_COLS - 1)))
+#ifdef STANDARD
+template <>
+	void convolution(cache_in &in, cache_ker &kernel, cache_out &out) {
+#pragma HLS inline
+		// find center position of kernel (half of kernel size)
+		const auto kCenterX = N_KER_COLS / 2;
+		const auto kCenterY = N_KER_ROWS / 2;
+
+I_LOOP:		for (auto i = 0; i < N_ROWS; ++i) {
+J_LOOP:			for (auto j = 0; j < N_COLS; ++j) {
+				auto tmp = 0;
+M_LOOP:				for (auto m = 0; m < N_KER_ROWS; ++m) {
+N_LOOP:					for (auto n = 0; n < N_KER_COLS; n += RD_PORTS) {
+#pragma HLS pipeline II=1
+PORT_LOOP:					for (auto port = 0; port < RD_PORTS; port++) {
+							// index of input signal, used for checking boundary
+							const auto ii = i + (kCenterY - m);
+							const auto jj = j + (kCenterX - (n + port));
+
+							// ignore input samples which are out of bound
+							if (((n + port) < N_KER_COLS) &&
+									((ii >= 0) && (ii < N_ROWS) && (jj >= 0) && (jj < N_COLS))) {
+								tmp += (in.get(ii * N_COLS + jj, port) *
+										kernel.get(m * N_KER_COLS + (n + port), port));
+							}
+						}
+						if ((m == (int)(N_KER_ROWS - 1)) && (n >= (int)(N_KER_COLS - RD_PORTS - 1)))
 							out[i * N_COLS + j] = tmp;
 					}
 				}
 			}
 		}
 	}
+#else
+template <>
+	void convolution(cache_in &in, cache_ker &kernel, cache_out &out) {
+#pragma HLS inline
+		// find center position of kernel (half of kernel size)
+		const auto kCenterX = N_KER_COLS / 2;
+		const auto kCenterY = N_KER_ROWS / 2;
+
+I_LOOP:		for (auto i = 0; i < N_ROWS; ++i) {
+J_LOOP:			for (auto j = 0; j < N_COLS; ++j) {
+				auto tmp = 0;
+M_LOOP:				for (auto m = 0; m < N_KER_ROWS; m += RD_PORTS) {
+N_LOOP:					for (auto n = 0; n < N_KER_COLS; ++n) {
+#pragma HLS pipeline II=1
+PORT_LOOP:					for (auto port = 0; port < RD_PORTS; port++) {
+							// index of input signal, used for checking boundary
+							const auto ii = i + (kCenterY - (m + port));
+							const auto jj = j + (kCenterX - n);
+
+							// ignore input samples which are out of bound
+							if ((m + port < N_KER_ROWS) &&
+									((ii >= 0) && (ii < N_ROWS) && (jj >= 0) && (jj < N_COLS))) {
+								tmp += (in.get(ii * N_COLS + jj, port) *
+										kernel.get((m + port) * N_KER_COLS + n, port));
+							}
+						}
+						if ((m >= (int)(N_KER_ROWS - RD_PORTS - 1)) && (n == (N_KER_COLS - 1)))
+							out[i * N_COLS + j] = tmp;
+					}
+				}
+			}
+		}
+	}
+#endif
 
 void convolution_syn(cache_in &in, cache_ker &kernel, cache_out &out) {
 #pragma HLS inline off
@@ -65,58 +132,91 @@ void convolution_syn(cache_in &in, cache_ker &kernel, cache_out &out) {
 	out.stop();
 }
 
+void convolution_syn(cache_in &in, int *kernel, cache_out &out) {
+#pragma HLS inline off
+	in.init();
+	out.init();
+
+	convolution<cache_in &, int *, cache_out &>(in, kernel, out);
+
+	in.stop();
+	out.stop();
+}
+
 extern "C" void convolution_top(int in[N_ROWS * N_COLS],
-		int kernel[N_KER_ROWS_ROUND * N_KER_COLS_ROUND], int out[N_ROWS * N_COLS]) {
-#pragma HLS INTERFACE m_axi port=in bundle=gmem0
-#pragma HLS INTERFACE m_axi port=out bundle=gmem1
-#pragma HLS interface m_axi port=kernel bundle=gmem2
+		int kernel[N_KER_ROWS * N_KER_COLS], int out[N_ROWS * N_COLS]) {
+#pragma HLS INTERFACE m_axi port=in offset=slave bundle=gmem0
+#pragma HLS INTERFACE m_axi port=kernel offset=slave bundle=gmem1
+#pragma HLS interface m_axi port=out offset=slave bundle=gmem2
+#pragma HLS INTERFACE ap_ctrl_hs port=return
 
 #if defined(CACHE)
 #pragma HLS dataflow disable_start_propagation
 	cache_in in_cache;
+#ifndef KER_BUFF
 	cache_ker ker_cache;
+#endif
 	cache_out out_cache;
-#pragma HLS disaggregate variable=in_cache
-#pragma HLS disaggregate variable=ker_cache
-#pragma HLS disaggregate variable=out_cache
 
 #ifdef __SYNTHESIS__
 	in_cache.run(in);
-	ker_cache.run(kernel);
 	out_cache.run(out);
+#ifdef KER_BUFF
+	convolution_syn(in_cache, kernel, out_cache);
+#else
+	ker_cache.run(kernel);
 	convolution_syn(in_cache, ker_cache, out_cache);
+#endif
 #else
 	in_cache.init();
-	ker_cache.init();
 	out_cache.init();
 
 	std::thread in_cache_thread([&]{in_cache.run(in);});
-	std::thread ker_cache_thread([&]{ker_cache.run(kernel);});
 	std::thread out_cache_thread([&]{out_cache.run(out);});
+#ifdef KER_BUFF
+	std::thread convolution_thread([&]{
+			convolution<cache_in &, int *, cache_out &>(in_cache,
+					kernel, out_cache);
+			});
+#else
+	ker_cache.init();
+	std::thread ker_cache_thread([&]{ker_cache.run(kernel);});
+
 	std::thread convolution_thread([&]{
 			convolution<cache_in &, cache_ker &, cache_out &>(in_cache,
 					ker_cache, out_cache);
 			});
+#endif
 
 	convolution_thread.join();
 
 	in_cache.stop();
-	ker_cache.stop();
 	out_cache.stop();
-	in_cache_thread.join();
+#ifndef KER_BUFF
+	ker_cache.stop();
 	ker_cache_thread.join();
+#endif
+	in_cache_thread.join();
 	out_cache_thread.join();
 
 #ifdef PROFILE
-	printf("in hit ratio = %.3f ((%d + %d) / %d)\n",
-			in_cache.get_hit_ratio(), in_cache.get_n_l1_hits(),
-			in_cache.get_n_hits(), in_cache.get_n_reqs());
-	printf("kernel hit ratio = %.3f ((%d + %d) / %d)\n",
-			ker_cache.get_hit_ratio(), ker_cache.get_n_l1_hits(),
-			ker_cache.get_n_hits(), ker_cache.get_n_reqs());
-	printf("out hit ratio = %.3f (%d / %d)\n",
-			out_cache.get_hit_ratio(), out_cache.get_n_hits(),
-			out_cache.get_n_reqs());
+	printf("IN hit ratio = \n");
+	for (auto port = 0; port < RD_PORTS; port++) {
+		printf("\tP=%d: L1=%d/%d; L2=%d/%d\n", port,
+				in_cache.get_n_l1_hits(port), in_cache.get_n_l1_reqs(port),
+				in_cache.get_n_hits(port), in_cache.get_n_reqs(port));
+	}
+#ifndef KER_BUFF
+	printf("Kernel hit ratio = \n");
+	for (auto port = 0; port < RD_PORTS; port++) {
+		printf("\tP=%d: L1=%d/%d; L2=%d/%d\n", port,
+				ker_cache.get_n_l1_hits(port), ker_cache.get_n_l1_reqs(port),
+				ker_cache.get_n_hits(port), ker_cache.get_n_reqs(port));
+	}
+#endif
+	printf("Out hit ratio = L1=%d/%d; L2=%d/%d\n",
+			out_cache.get_n_l1_hits(0), out_cache.get_n_l1_reqs(0),
+			out_cache.get_n_hits(0), out_cache.get_n_reqs(0));
 #endif /* PROFILE */
 #endif	/* __SYNTHESIS__ */
 #elif defined(GLOBAL)
@@ -139,10 +239,15 @@ extern "C" void convolution_top(int in[N_ROWS * N_COLS],
 }
 
 int main() {
-	int in[N_ROWS * N_COLS];
-	int kernel[N_KER_ROWS_ROUND * N_KER_COLS_ROUND];
-	int out[N_ROWS * N_COLS];
-	int out_ref[N_ROWS * N_COLS];
+	int *in;
+	int *kernel;
+	int *out;
+	int *out_ref;
+
+	in = (int *)malloc(N_ROWS * N_COLS * sizeof(int));
+	kernel = (int *)malloc(N_KER_ROWS * N_KER_COLS * sizeof(int));
+	out = (int *)malloc(N_ROWS * N_COLS * sizeof(int));
+	out_ref = (int *)malloc(N_ROWS * N_COLS * sizeof(int));
 
 	for (auto i = 0; i < (N_ROWS * N_COLS); i++)
 		in[i] = i;
@@ -153,10 +258,16 @@ int main() {
 	convolution_top(in, kernel, out);
 	convolution(in, kernel, out_ref);
 
+	int ret = 0;
 	for (auto i = 0; i < (N_ROWS * N_COLS); i++)
 		if (out[i] != out_ref[i])
-			return 1;
+			ret++;
 
-	return 0;
+	free(in);
+	free(kernel);
+	free(out);
+	free(out_ref);
+
+	return ret;
 }
 
--- test/matmul_test.cpp
+++ test/matmul_test.cpp
@@ -4,23 +4,146 @@
 #define PROFILE
 #include <thread>
 #endif	/* __SYNTHESIS__ */
-#include "matrix.h"
 #include "cache.h"
 
 #define CACHE
 //#define GLOBAL
 //#define LOCAL
 
+//#define NAIVE
+#define STANDARD
+
 #define N 16
 #define M 16
 #define P 16
 
-static const int RD_PORTS = 1;
-
+static const int RD_PORTS = 1; 
+static const int BLOCKS = 2;
 typedef int data_type;
-typedef cache<data_type, true, false, RD_PORTS, N * M, 1, 1, 16, false, 0, 1> cache_a;
-typedef cache<data_type, true, false, RD_PORTS, M * P, 1, 1, 16, false, 0, 1> cache_b;
-typedef cache<data_type, false, true, 1, N * P, 1, 1, 16, false, 0, 1> cache_c;
+typedef cache<data_type, true, false, RD_PORTS, N * M, 1, 1, BLOCKS, false, 0, 0, false, 7> cache_a;
+typedef cache<data_type, true, false, 1, M * P, 1, BLOCKS, BLOCKS, false, 0, 0, false, 7> cache_b;
+typedef cache<data_type, true, true, 1, N * P, 1, RD_PORTS, BLOCKS, false, 0, 0, false, 7> cache_c;
+
+template<typename T, typename U, typename V>
+void multiply(T A, U B, V C) {
+#pragma HLS inline
+MULT_I_LOOP:		for (int i = 0; i < N; i++) {
+MULT_J_LOOP:			for (int j = 0; j < P; j++) {
+					int acc = 0;
+MULT_K_LOOP:				for (int k = 0; k < M; k++) {
+#pragma HLS pipeline II=1
+#pragma HLS unroll factor=RD_PORTS
+						acc += A[i * M + k] * B[k * P + j];
+						if (k == (M - 1))
+							C[i * P + j] = acc;
+					}
+				}
+			}
+}
+
+#ifdef NAIVE
+#ifdef STANDARD
+// Naive Standard
+template<>
+void multiply(cache_a &A, cache_b &B, cache_c &C) {
+#pragma HLS inline
+MULT_I_LOOP:for (int i = 0; i < N; i++) {
+MULT_J_LOOP:	for (int j = 0; j < P; j++) {
+			int acc = 0;
+MULT_K_LOOP:		for (int k = 0; k < M; k += RD_PORTS) {
+#pragma HLS pipeline II=1
+PORT_LOOP:			for (auto port = 0; port < RD_PORTS; port++) {
+#pragma HLS unroll
+					acc += A.get(i * M + (k + port), port) * B.get((k + port) * P + j, port);
+				}
+			}
+			C[i * P + j] = acc;
+		}
+	    }
+}
+#else
+// Naive Optimized
+template<>
+void multiply(cache_a &A, cache_b &B, cache_c &C) {
+#pragma HLS inline
+MULT_I_LOOP:for (int i = 0; i < N; i += RD_PORTS) {
+MULT_J_LOOP:	for (int j = 0; j < P; j++) {
+			int acc[RD_PORTS] = {0};
+MULT_K_LOOP:		for (int k = 0; k < M; k++) {
+#pragma HLS pipeline II=1
+				const int b = B[k * P + j];
+MULTIPLY_LOOP:			for (auto port = 0; port < RD_PORTS; port++)
+					acc[port] += A.get((i + port) * M + k, port) * b;
+			}
+
+STORE_LOOP:		for (auto port = 0; port < RD_PORTS; port++) {
+#pragma HLS unroll
+				C[(i + port) * P + j] = acc[port];
+			}
+		}
+	}
+}
+#endif /* STANDARD */
+#else
+#ifdef STANDARD
+// Block Standard
+template<>
+void multiply(cache_a &A, cache_b &B, cache_c &C) {
+#pragma HLS inline
+JJ_LOOP:for (int jj = 0; jj < P; jj += BLOCKS) {
+KK_LOOP:	for (int kk = 0; kk < M; kk += BLOCKS) {
+I_LOOP:			for (int i = 0; i < N; i++) {
+J_LOOP:				for (int j = jj; j < (jj + BLOCKS); j++) {
+					int acc = 0;
+K_LOOP:					for (int k = kk; k < (kk + BLOCKS); k += RD_PORTS) {
+#pragma HLS pipeline II=1
+						for (int port = 0; port < RD_PORTS; port++) {
+#pragma HLS unroll
+							acc += A.get(i * M + k + port, port) *
+								B.get((k + port) * P + j, port);
+						}
+					}
+					int tmp = C[i * P + j];
+					C[i * P + j] = tmp + acc;
+				}
+			}
+		}
+	}
+}
+#else
+// Block Optimized
+template<>
+void multiply(cache_a &A, cache_b &B, cache_c &C) {
+#pragma HLS inline
+JJ_LOOP:for (int jj = 0; jj < P; jj += BLOCKS) {
+KK_LOOP:	for (int kk = 0; kk < M; kk += BLOCKS) {
+I_LOOP:			for (int i = 0; i < N; i += RD_PORTS) {
+J_LOOP:				for (int j = jj; j < (jj + BLOCKS); j++) {
+					int acc[RD_PORTS] = {0};
+					int tmp[RD_PORTS];
+LOAD_C_LOOP:				for (auto port = 0; port < RD_PORTS; port++) {
+#pragma HLS unroll
+						tmp[port] = C[(i + port) * P + j];
+					}
+
+K_LOOP:					for (int k = kk; k < (kk + BLOCKS); k++) {
+#pragma HLS pipeline II=1
+						const int b = B[k * P + j];
+MULT_LOOP:					for (int port = 0; port < RD_PORTS; port++)
+							acc[port] += A.get((i + port) * M + k, port) * b;
+					}
+
+STORE_C_LOOP:				for (auto port = 0; port < RD_PORTS; port++) {
+#pragma HLS unroll
+						C[(i + port) * P + j] = tmp[port] + acc[port];
+					}
+				}
+			}
+		}
+	}
+}
+#endif /* STANDARD */
+#endif /* NAIVE */
 
 void multiply_syn(cache_a &a_cache, cache_b &b_cache, cache_c &c_cache) {
 #pragma HLS inline off
@@ -28,8 +151,7 @@ void multiply_syn(cache_a &a_cache, cache_b &b_cache, cache_c &c_cache) {
 	b_cache.init();
 	c_cache.init();
 
-	matrix::multiply<cache_a &, cache_b &, cache_c &, N, M, P, RD_PORTS>
-			(a_cache, b_cache, c_cache);
+	multiply<cache_a &, cache_b &, cache_c &> (a_cache, b_cache, c_cache);
 
 	a_cache.stop();
 	b_cache.stop();
@@ -61,8 +183,8 @@ extern "C" void matmul_top(data_type a_arr[N * M], data_type b_arr[M * P], data_
 	std::thread a_thd([&]{a_cache.run(a_arr);});
 	std::thread b_thd([&]{b_cache.run(b_arr);});
 	std::thread c_thd([&]{c_cache.run(c_arr);});
-	std::thread matmul_thread([&]{matrix::multiply<cache_a &, cache_b &,
-			cache_c &, N, M, P, RD_PORTS>(a_cache, b_cache, c_cache);});
+	std::thread matmul_thread([&]{multiply<cache_a &, cache_b &, cache_c &>
+			(a_cache, b_cache, c_cache);});
 
 	matmul_thread.join();
 
@@ -74,19 +196,22 @@ extern "C" void matmul_top(data_type a_arr[N * M], data_type b_arr[M * P], data_
 	c_thd.join();
 
 #ifdef PROFILE
-	printf("A hit ratio = %.3f ((%d + %d) / %d)\n",
-			a_cache.get_hit_ratio(), a_cache.get_n_l1_hits(),
-			a_cache.get_n_hits(), a_cache.get_n_reqs());
-	printf("B hit ratio = %.3f ((%d + %d) / %d)\n",
-			b_cache.get_hit_ratio(), b_cache.get_n_l1_hits(),
-			b_cache.get_n_hits(), b_cache.get_n_reqs());
-	printf("C hit ratio = %.3f ((%d + %d) / %d)\n",
-			c_cache.get_hit_ratio(), c_cache.get_n_l1_hits(),
-			c_cache.get_n_hits(), c_cache.get_n_reqs());
+	printf("A hit ratio = \n");
+	for (auto port = 0; port < RD_PORTS; port++) {
+		printf("\tP=%d: L1=%d/%d; L2=%d/%d\n", port,
+				a_cache.get_n_l1_hits(port), a_cache.get_n_l1_reqs(port),
+				a_cache.get_n_hits(port), a_cache.get_n_reqs(port));
+	}
+	printf("B hit ratio = L1=%d/%d; L2=%d/%d\n",
+			b_cache.get_n_l1_hits(0), b_cache.get_n_l1_reqs(0),
+			b_cache.get_n_hits(0), b_cache.get_n_reqs(0));
+	printf("C hit ratio = L1=%d/%d; L2=%d/%d\n",
+			c_cache.get_n_l1_hits(0), c_cache.get_n_l1_reqs(0),
+			c_cache.get_n_hits(0), c_cache.get_n_reqs(0));
 #endif /* PROFILE */
 #endif	/* __SYNTHESIS__ */
 #elif defined(GLOBAL)
-	matrix::multiply<data_type *, data_type *, data_type *, N, M, P, RD_PORTS>(a_arr, b_arr, c_arr);
+	multiply<data_type *, data_type *, data_type *>(a_arr, b_arr, c_arr);
 #elif defined(LOCAL)
 	data_type a_local[N * M];
 	data_type b_local[M * P];
@@ -98,7 +223,7 @@ extern "C" void matmul_top(data_type a_arr[N * M], data_type b_arr[M * P], data_
 	memcpy(a_local, a_arr, (N * M * sizeof(data_type)));
 	memcpy(b_local, b_arr, (M * P * sizeof(data_type)));
 
-	matrix::multiply<data_type *, data_type *, data_type *, N, M, P, RD_PORTS>(a_local, b_local, c_local);
+	multiply<data_type *, data_type *, data_type *>(a_local, b_local, c_local);
 
 	memcpy(c_arr, c_local, (N * P * sizeof(data_type)));
 #endif
@@ -107,34 +232,25 @@ extern "C" void matmul_top(data_type a_arr[N * M], data_type b_arr[M * P], data_
 int main() {
 	data_type a_arr[N * M];
 	data_type b_arr[M * P];
-	data_type c_arr[N * P];
-	data_type c_arr_ref[N * P];
+	data_type c_arr[N * P] = {0};
+	data_type c_arr_ref[N * P] = {0};
 
-	matrix::generate_random<data_type>(a_arr, N, M);
-	matrix::generate_random<data_type>(b_arr, M, P);
+	for (auto i = 0; i < N * M; i++)
+		a_arr[i] = i;
+	for (auto i = 0; i < M * P; i++)
+		b_arr[i] = i;
 
 	// matrix multiplication with caches
 	matmul_top(a_arr, b_arr, c_arr);
 	// standard matrix multiplication
-	matrix::multiply<data_type *, data_type *, data_type *,
-		N, M, P, RD_PORTS>(a_arr, b_arr, c_arr_ref);
+	multiply<data_type *, data_type *, data_type *>(a_arr, b_arr, c_arr_ref);
 
-#ifdef DEBUG
-	std::cout << "A = " << std::endl;
-	matrix::print(a_arr, N, M);
-	std::cout << std::endl << "B = " << std::endl;
-	matrix::print(b_arr, M, P);
-	std::cout << std::endl << "C (reference) = " << std::endl;
-	matrix::print(c_arr_ref, N, P);
-	std::cout << std::endl << "C (cache) = " << std::endl;
-	matrix::print(c_arr, N, P);
-#endif /* DEBUG */
-
-	if (!matrix::compare<data_type *>(c_arr, c_arr_ref, N, P)) {
-		std::cerr << "Mismatch detected" << std::endl;
-		return -1;
+	auto err = 0;
+	for (auto i = 0; i < N * P; i++) {
+		if (c_arr[i] != c_arr_ref[i])
+			err++;
 	}
 
-	return 0;
+	return err;
 }
 
