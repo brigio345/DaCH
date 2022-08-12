52904d0951646728f372cd86cc11f519eecd57d3
Giovanni Brignone <brigio345@gmail.com>
2022-08-12 12:56:05 +0000

    test: update BitSort testbench

--- test/bitonic_sort_test.cpp
+++ test/bitonic_sort_test.cpp
@@ -7,15 +7,14 @@
 #endif	/* __SYNTHESIS__ */
 #include "cache.h"
 
-#define CACHE
-//#define GLOBAL
-//#define LOCAL
+//#define CACHE
+#define BASELINE
 
-#define N_BITS 7
+#define N_BITS 20
 #define N (1 << N_BITS)
 
 typedef int data_type;
-typedef cache<data_type, true, true, 1, N, 1, 2, 16, false, 1, 4> cache_a;
+typedef cache<data_type, true, true, 1, N, 1, 2, 64, false, 1, 1, false, 4> cache_a;
 
 void compare_and_swap(data_type &pos0, data_type &pos1, const bool dir) {
 #pragma HLS inline
@@ -79,27 +78,24 @@ extern "C" void bitonic_sort_top(data_type a_arr[N]) {
 	a_thd.join();
 
 #ifdef PROFILE
-	printf("A hit ratio = %.3f ((%d + %d) / %d)\n",
-			a_cache.get_hit_ratio(), a_cache.get_n_l1_hits(),
-			a_cache.get_n_hits(), a_cache.get_n_reqs());
+	printf("A hit ratio = L1: %d/%d L2: %d/%d\n",
+			a_cache.get_n_l1_hits(0), a_cache.get_n_l1_reqs(0),
+			a_cache.get_n_hits(0), a_cache.get_n_reqs(0));
 #endif /* PROFILE */
 #endif	/* __SYNTHESIS__ */
-#elif defined(GLOBAL)
+#elif defined(BASELINE)
 	bitonic_sort<data_type *>(a_arr);
-#elif defined(LOCAL)
-	data_type local_arr[N];
-
-	memcpy(local_arr, a_arr, (N * sizeof(data_type)));
-	bitonic_sort(local_arr);
-	memcpy(a_arr, local_arr, (N * sizeof(data_type)));
 #else
-	std::cout << "CACHE or GLOBAL or LOCAL must be defined" << std::endl;
+	std::cout << "CACHE or BASELINE must be defined" << std::endl;
 #endif
 }
 
 int main() {
-	data_type a_arr[N];
-	data_type a_arr_ref[N];
+	data_type *a_arr;
+	data_type *a_arr_ref;
+
+	a_arr = (data_type *)malloc(N * sizeof(data_type));
+	a_arr_ref = (data_type *)malloc(N * sizeof(data_type));
 
 	for (auto i = 0; i < N; i++) {
 		a_arr[i] = (std::rand() % 1000);
@@ -120,6 +116,9 @@ int main() {
 		if (a_arr[i] > a_arr[i - 1])
 			return 2;
 	}
+
+	free(a_arr);
+	free(a_arr_ref);
 
 	return 0;
 }
