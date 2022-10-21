c96cec6b001eb083dcfa45a41fccfb21db3deb3a
Giovanni Brignone <brigio345@gmail.com>
2022-10-21 10:00:29 +0200

    cleanup(test): remove outdated benchmarks

--- scripts/non_byte_access.tcl
+++
@@ -1,8 +0,0 @@
-source scripts/run_hls.tcl
-
-set top "non_byte_access"
-set design_files {"test/non_byte_access_test.cpp"}
-set tb_files {"test/non_byte_access_test.cpp"}
-
-run_hls $top $design_files $tb_files xcvu9p-flga2104-2-i
-
